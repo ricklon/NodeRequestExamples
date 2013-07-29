032fa60c97bdc09d51caea0a84ea3a525eda4ea4
Ricklon <rick.rickanderson@gmail.com>
2013-07-29 14:11:01 -0400

    updated with your key variable

--- HelloRottenTomatoes.js
+++ HelloRottenTomatoes.js
@@ -1,20 +1,22 @@
 var request = require('request');
+var key = "your key";
+
 /*
-request('http://api.rottentomatoes.com/api/public/v1.0.json?apikey=t7hguhwcm7d7nx5zpwjk4re9', function (error, response, body) {
+request('http://api.rottentomatoes.com/api/public/v1.0.json?apikey='+key, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body);
   }
 });
 
 
-request('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=t7hguhwcm7d7nx5zpwjk4re9', function (error, response, body) {
+request('http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey='+key, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     console.log(body);
   }
 });
 */
 
-request('http://api.rottentomatoes.com/api/public/v1.0/movies/769959054/similar.json?apikey=t7hguhwcm7d7nx5zpwjk4re9&q=night', function (error, response, body) {
+request('http://api.rottentomatoes.com/api/public/v1.0/movies/769959054/similar.json?apikey='+key+'&q=night', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     body = JSON.parse(body);
     console.log(body.movies[0].title);
